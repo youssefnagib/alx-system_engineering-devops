fafafd7e2278cd47cc9803deefb543ef6477dfdd
youssef nagib <youssefnagib14@yahoo.com>
2024-07-04 05:55:16 +0300

    poppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
 # install flask from pip3 with puppet.
 
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3',
-}
-
 package { 'flask':
   ensure   => '2.1.2',
   provider => 'pip3',
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => 'Package[Flask]',
 }
