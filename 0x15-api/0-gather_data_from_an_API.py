4d572df0527a7d4c3d887de0ec5b158967eeea85
youssef nagib <youssefnagib14@yahoo.com>
2024-08-13 19:38:53 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,20 +18,18 @@ if __name__ == '__main__':
         sys.exit(1)
 
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
+    user_url = "{}/users/{}".format(base_url, employee_id)
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
-        print(f"Error: Unable to retrieve user
-              information for employee ID {employee_id}.")
+        print("Error: Unable to retrieve user information for employee ID {}.".format(employee_id))
         sys.exit(1)
 
     user_data = user_response.json()
     employee_name = user_data['name']
-    todos_url = f"{base_url}/todos?userid={employee_id}"
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
     todos_response = requests.get(todos_url)
     if todos_response.status_code != 200:
-        print(f"Error: Unable to retrieve TODO list
-              for employee ID {employee_id}.")
+        print("Error: Unable to retrieve TODO list for employee ID {}.".format(employee_id))
         sys.exit(1)
 
     todos = todos_response.json()
@@ -39,7 +37,6 @@ if __name__ == '__main__':
     completed_tasks = [task for task in todos if task['completed']]
     number_of_completed_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks
-          ({number_of_completed_tasks}/{total_tasks}): ")
+    print("Employee {} is done with tasks ({}/{}):".format(employee_name, number_of_completed_tasks, total_tasks))
     for task in completed_tasks:
-        print(f"\t {task['title']}")
+        print("\t {}".format(task['title']))
